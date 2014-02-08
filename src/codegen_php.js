bf7c94a57f7a575077b4d402d77eeb2f6bb6d203
三冰 <shengyong.csy@taobao.com>
2014-02-08 14:28:28 +0800

    PHP 直译，而不是 function

--- src/codegen_php.js
+++ src/codegen_php.js
@@ -16,14 +16,16 @@ function codegen_php_tran(prog) {
 		s_output += s_indent + s + '\n';
 	}
 	function compileEval(stmt) {
-		var t = 'ToString(' + walkExpr(stmt[1]) + ')';
+		var t = walkExpr(stmt[1]);
 		if (stmt[2]) {
-			t = 'htmlspecialchars(' + t + ", ENT_COMPAT, 'GB2312')";
+			t = 'crox_encode(' + t + ')';
+		} else {
+			t = 'crox_ToString(' + t + ')';
 		}
-		emit('$t_r .= ' + t + ';');
+		emit('echo ' + t + ';');
 	}
 	function compileContent(stmt) {
-		emit('$t_r .= ' + phpQuote(stmt[1]) + ';');
+		emit('echo ' + phpQuote(stmt[1]) + ';');
 	}
 	function compileIf(stmt) {
 		emit('if(' + walkExpr(stmt[1]) + '){');
@@ -40,8 +42,8 @@ function codegen_php_tran(prog) {
 		}
 	}
 	function compileEach(stmt) {
-		var idKey = stmt[3] ? '$i_' + stmt[3] + '=>' : '';
-		emit('foreach(' + walkExpr(stmt[1]) + ' as ' + idKey + '$i_' + stmt[4] + ')');
+		var idKey = stmt[3] ? '$crox_' + stmt[3] + '=>' : '';
+		emit('foreach(' + walkExpr(stmt[1]) + ' as ' + idKey + '$crox_' + stmt[4] + ')');
 		emit('{');
 		indent();
 		compileStmts(stmt[2]);
@@ -49,7 +51,7 @@ function codegen_php_tran(prog) {
 		emit('}');
 	}
 	function compileSet(stmt) {
-		emit('$i_' + stmt[1] + ' = ' + walkExpr(stmt[2]) + ';');
+		emit('$crox_' + stmt[1] + ' = ' + walkExpr(stmt[2]) + ';');
 	}
 	function compileStmt(a) {
 		switch (a[0]) {
@@ -77,7 +79,7 @@ function codegen_php_tran(prog) {
 	function walkExpr(x) {
 		switch (x[0]) {
 			case 'id':
-				return '$i_' + x[1];
+				return '$crox_' + x[1];
 			case 'lit':
 				if (typeof x[1] == 'string')
 					return phpQuote(x[1]);
@@ -87,13 +89,13 @@ function codegen_php_tran(prog) {
 			case '[]':
 				return exprToStr(x[1], isMember) + '[' + walkExpr(x[2]) + ']';
 			case '!':
-				return '!ToBoolean(' + exprToStr(x[1], isUnary) + ')';
+				return '!crox_ToBoolean(' + exprToStr(x[1], isUnary) + ')';
 			case 'u-':
 				return '- ' + exprToStr(x[1], isUnary);
 			case '*': case '/': case '%':
 				return exprToStr(x[1], isMul) + x[0] + exprToStr(x[2], isUnary);
 			case '+':
-				return 'plus(' + exprToStr(x[1], null) + ', ' + exprToStr(x[2], null) + ')';
+				return 'crox_plus(' + exprToStr(x[1], null) + ', ' + exprToStr(x[2], null) + ')';
 			case '-':
 				return exprToStr(x[1], isAdd) + '- ' + exprToStr(x[2], isMul);
 			case '<': case '>': case '<=': case '>=':
@@ -102,56 +104,16 @@ function codegen_php_tran(prog) {
 				var op = x[0] == 'eq' ? '===' : '!==';
 				return exprToStr(x[1], isEquality) + op + exprToStr(x[2], isRel);
 			case '&&':
-				return 'logical_and(' + exprToStr(x[1], null) + ', ' + exprToStr(x[2], null) + ')';
+				return 'crox_logical_and(' + exprToStr(x[1], null) + ', ' + exprToStr(x[2], null) + ')';
 			case '||':
-				return 'logical_or(' + exprToStr(x[1], null) + ', ' + exprToStr(x[2], null) + ')';
+				return 'crox_logical_or(' + exprToStr(x[1], null) + ', ' + exprToStr(x[2], null) + ')';
 			default:
 				throw Error("unknown expr: " + x[0]);
 		}
 	}
 
-	var s_output = "$t_r = '';\n";
+	var s_output = "";
 	compileStmts(prog[1]);
 
-	return s_output;
-}
-function codegen_php_wrap(s) {
-	/// <param name="s" type="String"></param>
-	/// <returns type="String" />
-	var s_output = "function temp($i_root) {\n";
-	s_output += 'function isNumber($a) { return is_float($a) || is_int($a); }\n';
-	s_output += 'function plus($a, $b) {\
-if (isNumber($a) && isNumber($b)) {\
-	return $a + $b;\
-}\
-else {\
-	return ToString($a) . ToString($b);\
-}\
-}\n';
-	s_output += 'function logical_and($a, $b) { return $a ? $b : $a; }\n';
-	s_output += 'function logical_or($a, $b) { return $a ? $a : $b; }\n';
-	s_output += "function ToString($a) {\n\
-	if (is_string($a)) return $a;\n\
-	if (isNumber($a)) return (string)$a;\n\
-	if (is_bool($a)) return $a ? 'true' : 'false';\n\
-	if (is_null($a)) return 'null';\n\
-	if (is_array($a)) {\n\
-		$s = '';\n\
-		for ($i = 0; $i < count($a); ++$i) {\n\
-			if ($i > 0) $s .= ',';\n\
-			if (!is_null($a[$i]))\n\
-				$s .= ToString($a[$i]);\n\
-		}\n\
-		return $s;\n\
-	}\n\
-	return '[object Object]';\n\
-}\n";
-	s_output += 'function ToBoolean($a) {\n\
-	if (is_string($a)) return strlen($a) > 0;\n\
-	if (is_array($a) || is_object($a)) return true;\n\
-	return (bool)$a;\n\
-}\n';
-	s_output += s;
-	s_output += 'return $t_r;\n}';
 	return s_output;
 }
